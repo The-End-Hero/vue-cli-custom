5c1ea748b120df1ae69b01d026d2c32cf3e6a502
Xiping.wang <527409987@qq.com>
2017-12-04 19:33:52 +0800

    Update DateSelect.vue

--- src/components/DateSelect.vue
+++ src/components/DateSelect.vue
@@ -1,7 +1,8 @@
 <template>
   <button class="dateselect-wxp">{{name}}
-    <div class="" @click="checktype">{{title}}</div>
-    <i class="icon iconfont" v-if="isdown"></i>
+    <div class="dateselect-title" @click="checktype">{{title}}</div>
+    <!--<i class="icon iconfont" v-if="isdown"></i>-->
+    <i class="icon iconfont rili" @click="checktype"></i>
     <!--<div class="button-list" ref="buttonList" v-if="isshow">-->
     <!--<div v-for="site in from.list" v-on:click.stop="checked">{{site.msg}}</div>-->
     <!--&lt;!&ndash;<div>校园招聘</div>&ndash;&gt;-->
@@ -9,10 +10,10 @@
     <!--</div>-->
     <div class="dateinset" v-if="isdown" >
       <div class="title">
-        <span @click="change(2) ">去年</span>
-        <span @click="change(0)">上个月</span>
-        <span>{{title}}</span>
-        <span @click="change(1)">下个月</span>
+        <span @click="change(2)">去年</span>
+        <span @click="change(0)"><i class="icon iconfont" style="transform: rotate(90deg); color:#666666;"></i></span>
+        <span><span>{{nowtime.year}}年</span>&nbsp;&nbsp;&nbsp;<span>{{nowtime.month}}月</span></span>
+        <span @click="change(1)"><i class="icon iconfont"></i></span>
         <span @click="change(3)">明年</span>
       </div>
       <div class="dateinset-table">
@@ -26,7 +27,7 @@
           <span>日</span>
         </div>
         <div class="dateinset-table-day">
-          <span v-for="item in day">{{item.day}}</span>
+          <span @click="checkday" v-for="item in day" :style="item.source=='this'?'color: #666666;':item.source=='now'?'background: #0091EA;color:#FFFFFF;':''" :class="item.source=='this'?'cancheck':''">{{item.day}}</span>
         </div>
       </div>
     </div>
@@ -48,7 +49,7 @@
         name: null,
         isdown: true,
         day: [
-          {day: 123},
+          {day: 123,source : 'this'},
         ],
       }
     },
@@ -57,12 +58,19 @@
       console.log('this.isdown', this.isdown)
     },
     created() {
-
     },
     mounted() {
       this.init()
     },
     methods: {
+      checkday(el){
+        console.log(el.target)
+        if(el.target.className.indexOf("cancheck")>-1){
+          this.nowtime.day = el.target.innerHTML
+          this.getFirstDay(this.nowtime.year, this.nowtime.month)
+        }
+
+      },
       checktype() {
         this.isdown = !this.isdown
       },
@@ -96,18 +104,17 @@
         }else {
           this.inityear(type)
         }
-
         this.getFirstDay(this.nowtime.year, this.nowtime.month)
         this.title = this.nowtime.year + '年' + this.nowtime.month + '月' + this.nowtime.day + '日'
       },
       inityear(type){
         if(type == 2){
-        // 去年
+          // 去年
           this.nowtime.year = this.nowtime.year-1
           this.nowtime.month = this.nowtime.month
           this.nowtime.day = this.nowtime.day
         }else if(type == 3){
-        // 明年
+          // 明年
           this.nowtime.year = this.nowtime.year+1
           this.nowtime.month = this.nowtime.month
           this.nowtime.day = this.nowtime.day
@@ -279,7 +286,6 @@
             firArrlength = 7
             break;
         }
-
         let thisallday = this.getMonthDay(_year, _month)
         let beforethisallday
         let afterthisallday
@@ -298,36 +304,44 @@
         console.log(afterthisallday,'afterthisallday')
         const arr = []
         for (let i = 0; i < thisallday; i++) {
-          arr.push(
-            {day: i + 1}
-          )
+          if(i == this.nowtime.day-1 ){
+            arr.push(
+              {day: i+1, source:'now'}
+            )
+          }else {
+            arr.push(
+              {day: i + 1, source:'this'}
+            )
+          }
         }
         for (let i = 0; i < firArrlength - 1; i++) {
           arr.unshift(
-            {day: beforethisallday - i}
+            {day: beforethisallday - i, source:'before'}
           )
         }
         for (let i = 0; i < 42 - thisallday - (firArrlength - 1); i++) {
           arr.push(
-            {day: i + 1}
+            {day: i + 1,source:'after'}
           )
         }
         console.log(arr, 'arr')
         this.day = arr
         return allDay % 7;
       }
-
     },
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .dateselect-title{
+    padding-left: 10px;
+    text-align: left;
+  }
   span {
     display: inline-block;
     vertical-align: top;
   }
-
   .dateselect-wxp {
     display: block;
     width: 260px;
@@ -336,36 +350,52 @@
     border: 1px solid #444;
     position: relative;
   }
-
+  .dateselect-wxp .rili{
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 34px;
+    width: 36px;
+    line-height: 34px;
+    color: #C7C7C7;
+  }
   .dateinset {
     position: absolute;
-    width: 100%;
+    width: 293px;
     top: 36px;
     left: 0;
     z-index: 9999;
     background-color: #fff;
+    border: 1px solid #E4E4E4;
+    box-shadow: 0 1px 2px 0 #E4E4E4;
+    border-radius: 4px;
   }
-
   .title {
     font-size: 0px;
+    height: 36px;
+    line-height: 36px;
+    border-bottom: 1px solid #E4E4E4;
   }
-
   .title span {
     display: inline-block;
     vertical-align: top;
     font-size: 14px;
   }
-
   .dateinset-table {
     width: 100%;
-  }
+    box-sizing: border-box;
+    padding: 10px 20px;
 
+  }
   .dateinset-table-day {
     font-size: 0px;
   }
-
   .dateinset-table-day span {
     font-size: 14px;
-    width: 14.2%;
+    width: 36px;
+    height: 36px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #999999;
   }
 </style>
